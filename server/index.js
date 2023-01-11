b2773f19586c1faa85f195e586b45fc6b436f144
tanzhizz <tanzhizhen1982@gmail.com>
2023-01-12 01:40:14 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL =
+  "mongodb+srv://Tanzhizhen:Rsf4yfmUX6DeSUeR@cluster0.2bbf0.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("strictQuery", false);
