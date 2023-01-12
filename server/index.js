f09ff35a4eb1abea6376433060f3c3c27df36c9c
tanzhizz <tanzhizhen1982@gmail.com>
2023-01-13 01:19:05 +0800

    backend config

--- server/index.js
+++ server/index.js
@@ -3,24 +3,31 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+mongoose.set("strictQuery", true);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 //https://www.mongodb.com/cloud/atlas
 
 const CONNECTION_URL =
-  "mongodb+srv://Tanzhizhen:Rsf4yfmUX6DeSUeR@cluster0.2bbf0.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://tanzhiz:6piJsQJvuhqHKt4k@cluster0.2bbf0.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message));
-
-// mongoose.set("strictQuery", false);
+  .catch((error) => console.log(`${error} did not connect`));
